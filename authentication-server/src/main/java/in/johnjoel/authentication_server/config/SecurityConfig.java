864ab2583eb849b290e330fa0a8fad49cb12ed7d
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-20 21:27:14 +0800

    ...

---
+++ authentication-server/src/main/java/in/johnjoel/authentication_server/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package in.johnjoel.authentication_server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    dfd dfdfvk sfs ps sdfsd  lsdf sdfsf
+}
