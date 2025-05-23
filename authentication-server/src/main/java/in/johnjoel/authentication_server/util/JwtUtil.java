ffe8cf557f2d7151dde8dcb9082615ab6b2c74bf
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-24 07:57:47 +0800

    ...

--- authentication-server/src/main/java/in/johnjoel/authentication_server/util/JwtUtil.java
+++ authentication-server/src/main/java/in/johnjoel/authentication_server/util/JwtUtil.java
@@ -1,5 +1,25 @@
 package in.johnjoel.authentication_server.util;
 
+import lombok.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret.key}")
+    private String SECRET_KEY;
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> cliams = new HashMap<>();
+        return createToken(cliams, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> cliams, String username) {
+        SECRET_KEY
+    }
 
 }
