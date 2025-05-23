24bc2fc019f13ab519e5a6fc036be226af7dec9c
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-23 10:11:39 +0800

    ...

--- authentication-server/src/main/java/in/johnjoel/authentication_server/config/SecurityConfig.java
+++ authentication-server/src/main/java/in/johnjoel/authentication_server/config/SecurityConfig.java
@@ -1,11 +1,26 @@
 package in.johnjoel.authentication_server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-
-    dfd dfdfvk sfs ps sdfsd  lsdf sdfsf
-    fghfgh
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "register", "/send-reset-otp", "reset-password", "/logout")
+                        .permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .logout(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
 }
