3b96ffbc1badca57a2469969639b8e8527fc8902
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-24 14:08:21 +0800

    ...

--- authentication-server/src/main/java/in/johnjoel/authentication_server/util/JwtUtil.java
+++ authentication-server/src/main/java/in/johnjoel/authentication_server/util/JwtUtil.java
@@ -1,9 +1,12 @@
 package in.johnjoel.authentication_server.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,7 +22,13 @@ public class JwtUtil {
     }
 
     private String createToken(Map<String, Object> cliams, String username) {
-        SECRET_KEY
+        Jwts.builder()
+                .setClaims(cliams)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) //10hr expiration
+                . signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+
     }
 
 }
