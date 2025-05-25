2eb395800f71e88dd4fa2058f4629d1dc71a47a1
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-25 17:11:20 +0800

    ...

--- authentication-server/src/main/java/in/johnjoel/authentication_server/config/SecurityConfig.java
+++ authentication-server/src/main/java/in/johnjoel/authentication_server/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -77,5 +78,6 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return new ProviderManager(authenticationProvider);
     }
+
 
 }
