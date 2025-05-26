08033d9f15b3f78745ecae3cb948ab0e2b4bb224
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-26 15:26:07 +0800

    ...

--- authentication-server/src/main/java/in/johnjoel/authentication_server/service/EmailService.java
+++ authentication-server/src/main/java/in/johnjoel/authentication_server/service/EmailService.java
@@ -1,7 +1,6 @@
 package in.johnjoel.authentication_server.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,7 +9,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class EmailService {
-    @Value("${spring.mail.properties.mail.smtp.from}")
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}") // Use this instead of 'from' in smtp properties
     private String fromEmail;
 
     public void sendWelcomeEmail(String toEmail, String name) {
@@ -18,8 +20,7 @@ public class EmailService {
         message.setFrom(fromEmail);
         message.setTo(toEmail);
         message.setSubject("Welcome to our Platform");
-        message.setText("Hello "+name+" \n\nThanks for registering with us!\n\nRegards, \n Authify Team");
+        message.setText("Hello " + name + ",\n\nThanks for registering with us!\n\nRegards,\nAuthify Team");
+        mailSender.send(message);
     }
-
-
 }
