5dcf0413911c71eca411b9fcea1af2fc564d6a92
johnjoel061 <alfabetejohnjoel@gmail.com>
2025-05-26 15:10:59 +0800

    ...

--- authentication-server/src/main/java/in/johnjoel/authentication_server/service/EmailService.java
+++ authentication-server/src/main/java/in/johnjoel/authentication_server/service/EmailService.java
@@ -1,4 +1,25 @@
 package in.johnjoel.authentication_server.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class EmailService {
+    @Value("${spring.mail.properties.mail.smtp.from}")
+    private String fromEmail;
+
+    public void sendWelcomeEmail(String toEmail, String name) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject("Welcome to our Platform");
+        message.setText("Hello "+name+" \n\nThanks for registering with us!\n\nRegards, \n Authify Team");
+    }
+
+
 }
